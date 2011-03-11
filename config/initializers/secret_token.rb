2d381a807968e16a65666d026459d9581ca70486
zhengluo <zheng.luo@hmmh.de>
2011-03-11 11:23:57 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clmobile::Application.config.secret_token = '8e5be820acec09f5c9c49fa4c09e74e901ca68833cbf008824a2ff2835fc317edfcab106105fc9a0e03153be7b2c82b073c169c5128ad9c33b8e7b09a28338c6'
